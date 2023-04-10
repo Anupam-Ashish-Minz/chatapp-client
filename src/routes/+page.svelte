2a01a05620761482290b12fd1d8aa489821d105a
Anupam Ashish Minz <anupamminz300am@gmail.com>
2023-04-10 12:04:41 +0530

    added zod
    installed zod and changed messages and chatroom type of be infered from zod
    also added zod parse to fetch functions in the same

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,26 @@
 <script lang="ts">
 	import { onMount } from "svelte";
+	import { z } from "zod";
 
-	type Chatroom = {
-		id: number;
-		name: string;
-	};
+	const ChatroomSchema = z.object({
+		id: z.number(),
+		name: z.string()
+	});
+
+	type Chatroom = z.infer<typeof ChatroomSchema>;
+
+	const MessageSchema = z.object({
+		id: z.number(),
+		time: z.string(),
+		body: z.string(),
+		user_id: z.number(),
+		chatroom_id: z.number()
+	});
+
+	type Message = z.infer<typeof MessageSchema>;
 
 	let chatrooms: Chatroom[] = [];
-	let messages: any[] = [];
+	let messages: Message[] = [];
 
 	// this value is modified in mount
 	let room_selected = 0;
@@ -15,7 +28,7 @@
 	function fetchMessages(room_selected: number) {
 		fetch(`http://localhost:4000/api/messages/${room_selected}`)
 			.then((res) => res.json())
-			.then((data) => (messages = data ?? []))
+			.then((data) => (messages = z.array(MessageSchema).parse(data ?? [])))
 			.catch((err) => console.error(err));
 	}
 
@@ -27,7 +40,7 @@
 		fetch("http://localhost:4000/api/chatrooms")
 			.then((res) => res.json())
 			.then((data) => {
-				chatrooms = data;
+        chatrooms = z.array(ChatroomSchema).parse(data);
 				room_selected = data[0].id;
 			});
 	});
