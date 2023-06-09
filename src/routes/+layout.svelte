4fdc0f1ece426ab3123e8433abae6bf9e1221955
Anupam Ashish Minz <anupamminz300am@gmail.com>
2023-06-09 10:51:24 +0530

    added navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,45 @@
+<script lang="ts">
+	import { onMount, setContext } from "svelte";
+	import { writable } from "svelte/store";
+
+	const userID = writable(0);
+	let isDropDownOpen = false;
+  setContext('user_id', userID);
+
+	onMount(() => {
+		fetch("http://localhost:4000/api/profile", {
+			credentials: "include"
+		})
+			.then((data) => data.json())
+			.then((data) => {
+				if (data && data.id) {
+					userID.set(data.id);
+				}
+			});
+	});
+
+</script>
+
 <nav>
 	<a href="/">home</a>
-	<a href="/login">login</a>
-	<a href="/signup">signup</a>
+	{#if $userID != 0}
+		<button
+			on:click={() => {
+				isDropDownOpen = !isDropDownOpen;
+			}}>profile</button
+		>
+		{#if isDropDownOpen}
+			<div class="dropdown">
+				<div class="dropdown-text">
+					<button>logout</button>
+					<button>view full profile</button>
+				</div>
+			</div>
+		{/if}
+	{:else}
+		<a href="/login">login</a>
+		<a href="/signup">signup</a>
+	{/if}
 </nav>
 <slot />
 
@@ -17,5 +55,15 @@
 	nav > a {
 		color: inherit;
 		text-decoration: none;
+	}
+
+	.dropdown {
+		position: relative;
+		background-color: black;
+	}
+	.dropdown-text {
+		position: absolute;
+		background-color: inherit;
+		padding: 1rem;
 	}
 </style>
